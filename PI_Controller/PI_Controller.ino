e4713941bd21c4deab4c1c5e1deb90e8dbae4b13
RaghavpravinKS <raghavpravin.ks@gmail.com>
2024-06-23 17:12:03 +0530

    Added new fc algo

---
+++ PI_Controller/PI_Controller.ino
@@ -0,0 +1,492 @@
+#include <Servo.h>
+#include <RF24.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <nRF24L01.h>
+#include "MPU6050_res_define.h"
+// #include <math.h>
+#include <pwm.h>
+
+
+SPIClass SPI(0);
+TwoWire Wire(1);
+const int MPU6050 = 0x68;
+int redLed=24;    // Red LED
+int greenLed=22; // Green LED
+int blueLed=23;  // Blue LED
+
+#define TRUE 1
+#define FALSE 0
+
+uint32_t LoopTimer;
+
+RF24 receiver(9, 10);
+const byte address[6] = "101000";
+
+#define MOTOR_1 1
+#define MOTOR_2 2
+#define MOTOR_3 3
+#define MOTOR_4 4
+
+// Motor PWMs
+int Motor1 = 0;
+int Motor2 = 0;
+int Motor3 = 0;
+int Motor4 = 0;
+
+struct Signal {
+  int height;
+  int frwrvr;
+  int rgtlft;
+  int yaw;
+};
+
+struct Kalman {
+  float Q_angle; // Process noise variance for the accelerometer
+  float Q_bias;  // Process noise variance for the gyro bias
+  float R_measure; // Measurement noise variance
+  float angle; // The angle calculated by the Kalman filter - part of the 2x1 state vector
+  float bias; // The gyro bias calculated by the Kalman filter - part of the 2x1 state vector
+  float P[2][2]; // Error covariance matrix - This is a 2x2 matrix
+};
+
+Kalman kalmanX, kalmanY;
+float kalmanAngleRoll = 0, kalmanAnglePitch = 0;
+float kalman_uncertainity_roll = 2*2;
+float kalman_uncertainity_pitch = 2*2;
+float kalman_1d_output[] = {0, 0};
+
+Signal rx_data;
+
+// Complementary filter constant
+float alpha = 0.98;
+const float dt = 0.002;
+signed short Ax, Ay, Az, Tmp, Gx, Gy, Gz;
+float Ax_cal, Ay_cal, Az_cal, Tmp_cal, Gx_cal, Gy_cal, Gz_cal;
+float Ax_offset, Ay_offset, Az_offset, Tmp_offset, Gx_offset, Gy_offset, Gz_offset;
+
+float roll, pitch, rate_yaw;
+float roll_offset, pitch_offset, rate_yaw_offset;
+
+float roll_setpoint = 0;
+float pitch_setpoint = 0;
+float rate_yaw_setpoint = 0;
+
+float roll_error, pitch_error, rate_yaw_error;
+
+float roll_integral = 0;
+float pitch_integral = 0;
+float rate_yaw_integral = 0;
+
+float roll_derivative = 0;
+float pitch_derivative = 0;
+float rate_yaw_derivative = 0;
+
+float roll_output, pitch_output, rate_yaw_output;
+
+float Kp_roll = 125.0;
+float Ki_roll = 10;
+// float Kd_roll = 0.5;
+
+float Kp_pitch = 125.0;
+float Ki_pitch = 10;
+// float Kd_pitch = 0.5;
+
+float Kp_rate_yaw = 0.0;
+float Ki_rate_yaw = 0.0;
+// float Kd_yaw = 0.5;
+
+void setup(){
+    digitalWrite(blueLed, LOW);
+    delay(1000);
+    Serial.begin(115200);
+    // Wire.begin();
+    // Wire.setClock(400000);
+    Wire.beginTransmission(MPU6050);
+    Wire.write(SMPLRT_DIV);
+    Wire.write(0x07);
+    delayMicroseconds(100);
+    Wire.endTransmission(true);
+    delayMicroseconds(100);
+    Wire.beginTransmission(MPU6050);
+    Wire.write(PWR_MGMT_1);    
+    Wire.write(0x00);
+    Wire.endTransmission(true);
+    delayMicroseconds(100);
+    Wire.beginTransmission(MPU6050);
+    Wire.write(CONFIG);
+    Wire.write(0x00);
+    Wire.endTransmission(true);
+    delayMicroseconds(100);
+    Wire.beginTransmission(MPU6050);
+    Wire.write(GYRO_CONFIG);
+    Wire.write(0x08);
+    Wire.endTransmission(true);
+    delayMicroseconds(100);
+    Wire.beginTransmission(MPU6050);
+    Wire.write(ACCEL_CONFIG);
+    Wire.write(0x10);
+    Wire.endTransmission(true);
+    delayMicroseconds(100);
+
+    receiver.begin();
+    if (!receiver.begin()){
+        Serial.println("Receiver failed to start");
+        digitalWrite(redLed, LOW);
+        digitalWrite(blueLed, HIGH);
+    }
+    receiver.openReadingPipe(0, address);
+    receiver.setPALevel(RF24_PA_HIGH);
+    receiver.startListening();
+
+    int j,k;
+    digitalWrite(redLed,LOW);
+    for (int i=1;i<5;i++){
+        if (i%2==0){
+            k=9000;
+        }
+        else{
+            k=0;
+        }
+        for (j = 60000; j<104000;j+=1000){
+        
+            PWM.PWMC_Set_Period(i, SERVO_PERIOD);
+            PWM.PWMC_Set_OnOffTime(i, j+k);
+            PWM.PWMC_init(i);
+            PWM.PWMC_Enable();
+            delay(100);
+            digitalWrite(redLed,LOW);
+        }
+        PWM.PWMC_Set_Period(i, SERVO_PERIOD);
+        PWM.PWMC_Set_OnOffTime(i, 61000);
+        PWM.PWMC_init(i);
+        PWM.PWMC_Enable();
+        delay(500);
+
+    }
+    digitalWrite(redLed,HIGH);
+    delay(1000);
+
+    for (int i = 0; i < 2000; i++){
+        digitalWrite(greenLed, LOW);
+        Wire.beginTransmission(MPU6050);
+        Wire.write(ACCEL_XOUT_H);
+        Wire.endTransmission(false);
+        Wire.requestFrom(MPU6050, 14, true);
+        Ax = ((Wire.read() << 8) | Wire.read());
+        Ay = ((Wire.read() << 8) | Wire.read());
+        Az = ((Wire.read() << 8) | Wire.read());
+        Tmp = ((Wire.read() << 8) | Wire.read());
+        Gx = ((Wire.read() << 8) | Wire.read());
+        Gy = ((Wire.read() << 8) | Wire.read());
+        Gz = ((Wire.read() << 8) | Wire.read());
+        Ax_cal = (float)Ax/4096.0;
+        Ay_cal = (float)Ay/4096.0;
+        Az_cal = (float)Az/4096.0 + 0.06;
+        Tmp_cal = (float)Tmp/340.0 + 36.53;
+        Gx_cal = (float)Gx/65.5;
+        Gy_cal = (float)Gy/65.5;
+        Gz_cal = (float)Gz/65.5;
+        Ax_offset += Ax_cal;
+        Ay_offset += Ay_cal;
+        Az_offset += Az_cal;
+        Tmp_offset += Tmp_cal;
+        Gx_offset += Gx_cal;
+        Gy_offset += Gy_cal;
+        Gz_offset += Gz_cal;
+        roll_offset +=  atan(Ay_cal/sqrt(Ax_cal*Ax_cal+Az_cal*Az_cal))*(180/3.142);
+        pitch_offset += -atan(Ax_cal/sqrt(Ay_cal*Ay_cal+Az_cal*Az_cal))*(180/3.142);
+        delay(5);
+    }
+    Ax_offset /= 2000;
+    Ay_offset /= 2000;
+    Az_offset /= 2000;
+    Tmp_offset /= 2000;
+    Gx_offset /= 2000;
+    Gy_offset /= 2000;
+    Gz_offset /= 2000;
+    roll_offset /= 2000;
+    pitch_offset /= 2000;
+    rate_yaw_offset /= 2000;
+    Serial.println("Calibration done");
+    Serial.print("Ax_offset: ");
+    Serial.print(Ax_offset);
+    Serial.print(" Ay_offset: ");
+    Serial.print(Ay_offset);
+    Serial.print(" Az_offset: ");
+    Serial.print(Az_offset);
+    Serial.print(" Tmp_offset: ");
+    Serial.print(Tmp_offset);
+    Serial.print(" Gx_offset: ");
+    Serial.print(Gx_offset);
+    Serial.print(" Gy_offset: ");
+    Serial.print(Gy_offset);
+    Serial.print(" Gz_offset: ");
+    Serial.print(Gz_offset);
+    Serial.print(" Roll_offset: ");
+    Serial.print(roll_offset);
+    Serial.print(" Pitch_offset: ");
+    Serial.println(pitch_offset);
+    digitalWrite(greenLed, HIGH);
+
+    // Initialize Kalman filters
+    initKalman(kalmanX);
+    initKalman(kalmanY);
+
+    for (int l = 0; l < 5; l++){
+        PWM.PWMC_Set_Period(l, SERVO_PERIOD);
+        PWM.PWMC_Set_OnOffTime(l, 0);
+        PWM.PWMC_init(l);
+        PWM.PWMC_Enable();
+        delay(100);
+        digitalWrite(redLed, LOW);
+    }
+    delay(1000);
+
+    digitalWrite(redLed,HIGH);
+    delay(1000);
+    digitalWrite(blueLed,HIGH);
+    delay(1000);
+}
+
+unsigned long a1 = 0;
+unsigned long a2 = 0;
+unsigned long a3 = 0;
+
+void loop() {
+    a1 = micros();
+    if (receiver.available()) {
+        digitalWrite(redLed,HIGH);
+        digitalWrite(greenLed,LOW);
+        digitalWrite(blueLed,HIGH);
+        receiver.read(&rx_data, sizeof(rx_data));
+
+    } else {
+        digitalWrite(redLed,LOW);
+        digitalWrite(greenLed,HIGH);
+        digitalWrite(blueLed,HIGH);
+        rx_data.height = rx_data.height;
+        rx_data.frwrvr = rx_data.frwrvr;
+        rx_data.rgtlft = rx_data.rgtlft;
+        rx_data.yaw = rx_data.yaw;
+    }
+
+
+    // a1 = micros();
+    Wire.beginTransmission(MPU6050);
+    Wire.write(ACCEL_XOUT_H);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU6050, 14, true);
+    Ax = ((Wire.read() << 8) | Wire.read());
+    Ay = ((Wire.read() << 8) | Wire.read());
+    Az = ((Wire.read() << 8) | Wire.read());
+    Tmp = ((Wire.read() << 8) | Wire.read());
+    Gx = ((Wire.read() << 8) | Wire.read());
+    Gy = ((Wire.read() << 8) | Wire.read());
+    Gz = ((Wire.read() << 8) | Wire.read());
+    Wire.endTransmission(true);
+
+    Ax_cal = (float)Ax/4096.0 ;
+    Ay_cal = (float)Ay/4096.0 ;
+    Az_cal = (float)Az/4096.0 + 0.06;
+    Tmp_cal = (float)Tmp/340.0 + 36.53;
+    Gx_cal = (float)Gx/65.5 - Gx_offset;
+    Gy_cal = (float)Gy/65.5 - Gy_offset;
+    Gz_cal = (float)Gz/65.5 - Gz_offset;
+
+    float current_roll =  atan(Ay_cal/sqrt(Ax_cal*Ax_cal+Az_cal*Az_cal))*(180/3.142) - roll_offset;
+    float current_pitch= -atan(Ax_cal/sqrt(Ay_cal*Ay_cal+Az_cal*Az_cal))*(180/3.142) - pitch_offset;
+
+    float com_roll = 0.98*(roll + Gx_cal*dt) + (1-alpha)*current_roll;
+    float com_pitch = 0.98*(pitch + Gy_cal*dt) + (1-alpha)*current_pitch;
+    // a2 = micros();
+    float kal_roll = kalmanFilter(kalmanX, current_roll, Gx_cal, dt);
+    float kal_pitch = kalmanFilter(kalmanY, current_pitch, Gy_cal, dt);
+
+    kalman_1d(kalmanAngleRoll, kalmanAnglePitch, Gx_cal, current_roll);
+    kalmanAngleRoll = kalman_1d_output[0];
+    kalman_uncertainity_roll = kalman_1d_output[1];
+
+    kalman_1d(kalmanAnglePitch, kalmanAnglePitch, Gy_cal, current_pitch);
+    kalmanAnglePitch = kalman_1d_output[0];
+    kalman_uncertainity_pitch = kalman_1d_output[1];
+
+    roll = com_roll;
+    pitch = com_pitch;
+    // roll = (1/2.05)*(1.05*kalmanAngleRoll + kal_roll);
+    // pitch = (1/2.05)*(1.05*kalmanAnglePitch + kal_pitch);
+    // roll = kalmanAngleRoll;
+    // pitch = kalmanAnglePitch;
+    rate_yaw = Gz_cal;
+
+    // Add dead zone
+    if (abs(roll) < 1.5) roll = 0;
+    if (abs(pitch) < 1.5) pitch = 0;
+    if (abs(rate_yaw) < 0.2) rate_yaw = 0;
+
+    roll_error = roll_setpoint - roll;
+    pitch_error = pitch_setpoint - pitch;
+    rate_yaw_error = rate_yaw_setpoint - rate_yaw;
+
+    // PI Controller for Roll
+    if (rx_data.height > 102000)roll_integral += roll_error;
+    if (abs(roll_integral) > 650)roll_integral = 650*(roll_integral/abs(roll_integral));
+    roll_output = Kp_roll * roll_error + Ki_roll * roll_integral;
+
+    // PI Controller for Pitch
+    if (rx_data.height > 102000)pitch_integral += pitch_error;
+    if (abs(pitch_integral) > 650)pitch_integral = 650*(pitch_integral/abs(pitch_integral));
+    pitch_output = Kp_pitch * pitch_error + Ki_pitch * pitch_integral;
+
+    // PI Controller for Rate Yaw
+    // if (rx_data.height > 100000) 
+    rate_yaw_integral += rate_yaw_error;
+    if (abs(rate_yaw_integral) > 650)rate_yaw_integral = 650*(rate_yaw_integral/abs(rate_yaw_integral));
+    rate_yaw_output = Kp_rate_yaw * rate_yaw_error + Ki_rate_yaw * rate_yaw_integral;
+
+    // Motor 1 = Height - Pitch + Rate Yaw - Roll
+    // Motor 2 = Height + Pitch - Rate Yaw - Roll
+    // Motor 3 = Height + Pitch + Rate Yaw + Roll
+    // Motor 4 = Height - Pitch - Rate Yaw + Roll
+
+    // Motor 1
+    Motor1 = rx_data.height * 0.920 - pitch_output + rate_yaw_output - roll_output;
+    if (Motor1 > 127000)Motor1 = 127000;
+    // Motor1 = Motor1;
+    if (Motor1 < 0)Motor1 = 0;
+    PWM.PWMC_Set_Period(1, SERVO_PERIOD);
+    PWM.PWMC_Set_OnOffTime(1, Motor1);
+    PWM.PWMC_init(1);
+    PWM.PWMC_Enable();
+    delayMicroseconds(100);
+
+    // Motor 2
+    Motor2 = rx_data.height * 1.030 + pitch_output - rate_yaw_output - roll_output;
+    if (Motor2 > 127000)Motor2 = 127000;
+    // Motor2 = Motor2 ;
+    if (Motor2 < 0)Motor2 = 0;
+    PWM.PWMC_Set_Period(2, SERVO_PERIOD);
+    PWM.PWMC_Set_OnOffTime(2, Motor2);
+    PWM.PWMC_init(2);
+    PWM.PWMC_Enable();
+    delayMicroseconds(100);
+
+    // Motor 3
+    Motor3 = rx_data.height * 0.920 + pitch_output + rate_yaw_output + roll_output;
+    if (Motor3 > 127000)Motor3 = 127000;
+    // Motor3 = Motor3 ;
+    if (Motor3 < 0)Motor3 = 0;
+    PWM.PWMC_Set_Period(3, SERVO_PERIOD);
+    PWM.PWMC_Set_OnOffTime(3, Motor3);
+    PWM.PWMC_init(3);
+    PWM.PWMC_Enable();
+    delayMicroseconds(100);
+
+    // Motor 4
+    Motor4 = rx_data.height * 1.0090 - pitch_output - rate_yaw_output+ roll_output;
+    if (Motor4 > 127000)Motor4 = 127000;
+    // Motor4 = Motor4 ;
+    if (Motor4 < 0)Motor4 = 0;
+    PWM.PWMC_Set_Period(4, SERVO_PERIOD);
+    PWM.PWMC_Set_OnOffTime(4, Motor4);
+    PWM.PWMC_init(4);
+    PWM.PWMC_Enable();
+    delayMicroseconds(100);
+
+    debugPrint();
+    a2 = micros();
+    if (a2-a1 < 4000) delayMicroseconds(4000-(a2-a1));
+    // a3 = micros();
+    // Serial.println(a2-a1);
+}
+
+
+void debugPrint() {
+    // Serial.print("Ax: ");
+    // Serial.print((Ax_cal)*9.8);
+    // Serial.print(" Ay: ");
+    // Serial.print((Ay_cal)*9.8);
+    // Serial.print(" Az: ");
+    // Serial.print(Az_cal*9.8);
+    // Serial.print(" Tmp: ");
+    // Serial.print(Tmp_cal);
+    // Serial.print(" Gx: ");
+    // Serial.print(Gx_cal);
+    // Serial.print(" Gy: ");
+    // Serial.print(Gy_cal);
+    // Serial.print(" Gz: ");
+    // Serial.print(Gz_cal);
+    // Serial.print(" Height: ");
+    // Serial.print(rx_data.height);
+    Serial.print(" Roll: ");
+    Serial.print(roll);
+    Serial.print(" Pitch: ");
+    Serial.print(pitch);
+    Serial.print(" Rate Yaw: ");
+    Serial.print(rate_yaw);
+    Serial.print(" Motor1: ");
+    Serial.print(Motor1);
+    Serial.print(" Motor2: ");
+    Serial.print(Motor2);
+    Serial.print(" Motor3: ");
+    Serial.print(Motor3);
+    Serial.print(" Motor4: ");
+    Serial.print(Motor4);
+    Serial.println();
+}
+
+void initKalman(Kalman &kalman) {
+  kalman.Q_angle = 0.007;
+  kalman.Q_bias = 0.007;
+  kalman.R_measure = 0.05;
+  kalman.angle = 0;
+  kalman.bias = 0;
+  kalman.P[0][0] = 0;
+  kalman.P[0][1] = 0;
+  kalman.P[1][0] = 0;
+  kalman.P[1][1] = 0;
+}
+
+
+float kalmanFilter(Kalman &kalman, float newAngle, float newRate, float dt) {
+  // Predict
+  float rate = newRate - kalman.bias;
+  kalman.angle += dt * rate;
+
+  kalman.P[0][0] += dt * (dt * kalman.P[1][1] - kalman.P[0][1] - kalman.P[1][0] + kalman.Q_angle);
+  kalman.P[0][1] -= dt * kalman.P[1][1];
+  kalman.P[1][0] -= dt * kalman.P[1][1];
+  kalman.P[1][1] += kalman.Q_bias * dt;
+
+  // Update
+  float y = newAngle - kalman.angle;
+  float S = kalman.P[0][0] + kalman.R_measure;
+  float K[2];
+  K[0] = kalman.P[0][0] / S;
+  K[1] = kalman.P[1][0] / S;
+
+  kalman.angle += K[0] * y;
+  kalman.bias += K[1] * y;
+
+  float P00_temp = kalman.P[0][0];
+  float P01_temp = kalman.P[0][1];
+
+  kalman.P[0][0] -= K[0] * P00_temp;
+  kalman.P[0][1] -= K[0] * P01_temp;
+  kalman.P[1][0] -= K[1] * P00_temp;
+  kalman.P[1][1] -= K[1] * P01_temp;
+
+  return kalman.angle;
+}
+
+void kalman_1d(float KalmanState, float KalmanUncertainty, float KalmanInput, float KalmanMeasurement) {
+  KalmanState=KalmanState+0.004*KalmanInput;
+  KalmanUncertainty=KalmanUncertainty + 0.004 * 0.004 * 4 * 4;
+  float KalmanGain=KalmanUncertainty * 1/(1*KalmanUncertainty + 3 * 3);
+  KalmanState=KalmanState+KalmanGain * (KalmanMeasurement-KalmanState);
+  KalmanUncertainty=(1-KalmanGain) * KalmanUncertainty;
+  kalman_1d_output[0]=KalmanState; 
+  kalman_1d_output[1]=KalmanUncertainty;
+
+
+}
